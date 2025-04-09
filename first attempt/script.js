7296f4e4e852353565e2ffbae098ba57af6c8752
Yobra28 <briankuruui3768@gmail.com>
2025-04-09 18:19:55 +0300

    ..

--- first attempt/script.js
+++ first attempt/script.js
@@ -2,19 +2,6 @@ const modalOverlay = document.getElementById('modalOverlay');
 const ratingScale = document.getElementById('ratingScale');
 let selectedRating = null;
 
-// Define inline SVG icons (customize paths as needed)
-const normalIcon = `<svg width="24" height="24" viewBox="0 0 24 24">
-  <path fill="currentColor" d="M4,4 L10,4 L10,6 L6,6 L6,10 L4,10 Z M14,4 L20,4 L20,10 L18,10 L18,6 L14,6 Z M4,14 L6,14 L6,18 L10,18 L10,20 L4,20 Z M14,20 L14,18 L18,18 L18,14 L20,14 L20,20 Z"/>
-</svg>`;
-
-const fullscreenIcon = `<svg width="24" height="24" viewBox="0 0 24 24">
-  <path fill="currentColor" d="M2,2 L2,10 L4,10 L4,4 L10,4 L10,2 Z M14,2 L14,4 L20,4 L20,10 L22,10 L22,2 Z M2,14 L4,14 L4,20 L10,20 L10,22 L2,22 Z M14,22 L14,20 L18,20 L18,14 L22,14 L22,22 Z"/>
-</svg>`;
-
-const smallIcon = `<svg width="24" height="24" viewBox="0 0 24 24">
-  <path fill="currentColor" d="M8,8 L16,8 L16,16 L8,16 Z"/>
-</svg>`;
-
 // Modal state can be "normal", "fullscreen", or "small"
 let modalState = "normal";
 
